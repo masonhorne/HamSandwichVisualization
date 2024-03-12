c76e3c1c019c3a8b768b2957bf27c45b293870d9
Mason Horne <masonhorne1@gmail.com>
2024-03-12 16:05:01 -0400

    add 3d option with button

--- script.ts
+++ script.ts
@@ -10,31 +10,60 @@ scene.background = new THREE.Color(0xFFFFFF);
 
 
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.zoomToCursor = true;
+controls.minDistance = 10;
+controls.maxDistance = 150;
 
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+geometry.translate(25, 25, 25);
 
+// camera.position.z = 10;
 camera.position.z = 10;
+
+controls.update();
 
 function animate() {
 	requestAnimationFrame( animate );
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+	controls.update();
 	renderer.render( scene, camera );
 }
 
 animate();
 
-// 2D Grid Setup
-const size = 10;
-const divisions = 10;
+// Grid Settings
+const size = 100;
+const divisions = 100;
 const axisColor = new THREE.Color(0x000000);
 const gridColor = new THREE.Color(0xD3D3D3);
+
+// 2D Grid Setup
 const gridHelper = new THREE.GridHelper( size, divisions, axisColor, gridColor );
 gridHelper.rotation.x = Math.PI / 2;
 scene.add( gridHelper );
 
-// TODO: Grid movement controls
+// 3D Grid Setup
+const gridHelperXZ = new THREE.GridHelper( size, divisions, axisColor, gridColor );
+scene.add( gridHelperXZ );
+const gridHelperYZ = new THREE.GridHelper( size, divisions, axisColor, gridColor );
+gridHelperYZ.rotation.z = Math.PI / 2;
+scene.add( gridHelperYZ );
+gridHelperXZ.visible = false;
+gridHelperYZ.visible = false;
+
+// 3D Control Button
+const threeDButton: any = document.getElementById('3d');
+let in3dMode: boolean = false;
+threeDButton.addEventListener('click', () => {
+	in3dMode = !in3dMode;
+	if(in3dMode) {
+		gridHelperXZ.visible = true;
+		gridHelperYZ.visible = true;
+	} else {
+		gridHelperXZ.visible = false;
+		gridHelperYZ.visible = false;
+	}
+});
