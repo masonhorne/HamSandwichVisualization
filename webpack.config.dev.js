08c31895c854638a828ce2b923fdbf4d7f4816d9
Mason Horne <masonhorne1@gmail.com>
2024-04-16 19:31:27 -0400

    move code into src directory

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './app.ts',
+	entry: './src/app.ts',
 	mode: 'development',
 	watch: true,
 	module: {
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-	entry: './app.ts',
+	entry: './src/app.ts',
 	mode: 'production',
 	module: {
 		rules: [
